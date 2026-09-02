d52f14c2a378d3ffaf27d63cf9536b1276ad6218
Vitório Mota <154383083+vitorio81@users.noreply.github.com>
2026-09-02 18:46:24 -0300

    feat: adiciona docker-compose e atualiza configuracoes de ambiente do backend

--- back_end/build/config/env.js
+++ back_end/build/config/env.js
@@ -34,28 +34,24 @@ var __importStar = (this && this.__importStar) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-// config/env.ts
 const dotenv = __importStar(require("dotenv"));
 const path = __importStar(require("path"));
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({
+    path: path.resolve(__dirname, "../../.env"),
+});
 exports.config = {
-    // Dados internos do back-end
     port: process.env.PORT || 3000,
     jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d",
     jwtSecret: process.env.JWT_SECRET || "chave_super_secreta",
-    // URL do front-end para CORS
     urlApiFront: process.env.URL_API_FRONT || "http://localhost:3000",
-    // Configurações do banco de dados
     dbRootUser: process.env.DB_ROOT_USER || "root",
     dbRootPassword: process.env.DB_ROOT_PASSWORD || "",
-    // Configurações do banco de dados para o usuário da aplicação
     dbHost: process.env.DB_HOST || "localhost",
     dbUser: process.env.DB_APP_USER || "educa_app",
     dbPassword: process.env.DB_APP_PASSWORD || "app123",
     database: process.env.DB_NAME || "educa_ar",
     waitForConnections: true,
     connectionLimit: 10,
-    // Configurações do Cloudflare R2
     bucketName: process.env.BUCKET_NAME,
     accountId: process.env.ACCOUNT_ID,
     accessKey: process.env.ACCESS_KEY,
